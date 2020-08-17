e8a6e4b36661e9639a9b36bbe5ba8faab5df55b7
Austin Barrett <virus.cmd@gmail.com>
2020-08-16 20:12:28 -0400

    blm banner

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="main-content">
+    <section class="has-text-centered has-background-dark has-text-white">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p style="margin: 5px 0">Support <a style="text-decoration: underline" class="has-text-warning has-text-white is-underline"
+                          href="https://blacklivesmatter.com/">#BlackLivesMatter</a> and the
+              <a style="text-decoration: underline" class="has-text-warning has-text-white is-underline"
+                 href="https://support.eji.org/give/153413/#!/donation/checkout">Equal Justice Initiative</a>.</p>
+
+          </div>
+        </div>
+      </div>
+    </section>
     <nuxt/>
     <section class="footer has-background-dark has-text-light"><div class="container">
       <footer>
